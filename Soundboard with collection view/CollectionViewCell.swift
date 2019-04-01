cd3573c423346b4481f2af2b81b20cb9da8b0269
Alex <alexander.p.niehaus@gmail.com>
2019-04-01 12:36:13 +0200

    fixed array with file

--- Soundboard with collection view/CollectionViewCell.swift
+++ Soundboard with collection view/CollectionViewCell.swift
@@ -10,5 +10,6 @@ import UIKit
 
 class CollectionViewCell: UICollectionViewCell {
     
+    @IBOutlet weak var buttonOutlet: UIButton!
     @IBOutlet weak var cellLabel: UILabel!
 }
--- Soundboard with collection view/ViewController.swift
+++ Soundboard with collection view/ViewController.swift
@@ -7,16 +7,48 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 class ViewController: UIViewController {
     
-    var sounds : [Sounds] = [Sounds(statement: "A"), Sounds(statement: "B"), Sounds(statement: "C"), Sounds(statement: "D")]
-//                            [Sounds(statement: "A", file: Bundle.main.url(forResource: "A", withExtension: "aifc")!),
-//                            Sounds(statement: "B", file: Bundle.main.url(forResource: "B", withExtension: "aifc")!),
-//                            Sounds(statement: "C", file: Bundle.main.url(forResource: "C", withExtension: "aifc")!),
-//                            Sounds(statement: "D", file: Bundle.main.url(forResource: "D", withExtension: "aifc")!)]
-//
+    var audioPlayer:AVAudioPlayer?
+    
+    lazy var sounds : [Sounds] =
+//        [Sounds(statement: "A"), Sounds(statement: "B"), Sounds(statement: "C"), Sounds(statement: "D")]
+                            [Sounds(statement: "A", file: Bundle.main.url(forResource: "A", withExtension: "aifc")),
+                            Sounds(statement: "B", file: Bundle.main.url(forResource: "B", withExtension: "aifc")),
+                            Sounds(statement: "C", file: Bundle.main.url(forResource: "C", withExtension: "aifc")),
+                            Sounds(statement: "D", file: Bundle.main.url(forResource: "D", withExtension: "aifc"))]
+
     @IBOutlet weak var collectionView: UICollectionView!
+    @IBAction func playSound(_ sender: Any) {
+        let urlA = Bundle.main.url(forResource: "A", withExtension: "aifc")
+        
+        guard urlA != nil else {
+            return
+        }
+
+        do{
+            audioPlayer = try AVAudioPlayer(contentsOf: urlA!)
+            audioPlayer?.play()
+            print("audio is playing")
+        } catch {
+            print("error")
+        }
+        
+        //1
+//        let buttonPosition: CGPoint = sender.location(in: self.collectionView)
+//        guard let indexPath = self.collectionView?.indexPathForItem(at: buttonPosition) else { return }
+//
+//        print ("doubleTap on cell at: ", indexPath)
+        
+        
+        //2
+        //let cell = self.collectionView.cellForItem(at: indexPath)
+        // now you have the cell and have access to the button
+
+        }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,9 +56,9 @@ class ViewController: UIViewController {
         let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
         layout.itemSize = CGSize(width: width, height: width)
         
-        print(sounds)
-    }
-
+        }
+    
+    let sender : Any? = nil
 }
 
 extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
@@ -35,14 +67,17 @@ extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        func cellTapped(selectedIndex: IndexPath) {
+//            let cellSelected = Int(selectedIndex)
+//            print("Selected cell named: \(sounds[selectedIndex].statement)")
+//        }
+        
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "soundCell", for: indexPath) as! CollectionViewCell
         let Soundz = sounds[indexPath.item]
-        
         cell.cellLabel.text = Soundz.statement
         
         return cell
     }
-    
-    
+
 }
 
